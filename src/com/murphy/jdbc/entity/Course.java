51a372894b3a137cfd20912ad7614a3b9d69dc6e
Murphy Hsieh <murphyhsieh@leandev.io>
2020-09-01 14:19:40 +0800

    retreiving courses associated to the instructor, REMEMBER add the default constructor of Course

--- src/com/murphy/jdbc/entity/Course.java
+++ src/com/murphy/jdbc/entity/Course.java
@@ -29,6 +29,9 @@ public class Course {
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
 
+    public Course() {
+    }
+
     public Course(String title) {
         this.title = title;
     }
